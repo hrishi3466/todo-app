9269aa75defe5bdf777f9cc31248f7fb24faa4bb
hrishi3466 <hrishi0626@gmail.com>
2026-06-29 14:32:14 +0530

    Fix backend Java compatibility

--- example.todo/example.todo/src/main/java/com/example/todo/model/Todo.java
+++ example.todo/example.todo/src/main/java/com/example/todo/model/Todo.java
@@ -6,15 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
-@NoArgsConstructor
 
 public class Todo {
     public Todo(Long id, String title, boolean completed, LocalDateTime createdAt, LocalDateTime updatedAt) {
